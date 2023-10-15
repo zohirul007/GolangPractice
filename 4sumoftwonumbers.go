e5b9b2a63a9430d276e66cce0445451eb847fb55
Zohirul Haque <zhremon@outlook.com>
2023-10-16 00:15:43 +0600

    sum of two numbers

---
+++ 4sumoftwonumbers.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n1, n2, result = 10, 3, 0 // Declare variable and assign values
+	result = n1 + n2
+	fmt.Println("Sum of two numbers: ", result)
+
+}
