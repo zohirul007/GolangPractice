1c1098e3bcd46b480cf2b75b5342699ea499cec3
Zohirul Haque <zhremon@outlook.com>
2023-10-16 21:00:25 +0600

    sum input from user

--- 5sumInputFromUser.go
+++ 5sumInputFromUser.go
@@ -6,10 +6,10 @@ func main() {
 
 	fmt.Print("Please enter first number: ")
 	var n1 int
-	fmt.Scanln(&n1) //Take input from user
+	fmt.Scanln(& n1) //Take input from user
 	fmt.Print("Please enter Second number: ")
 	var n2 int
-	fmt.Scanln(&n2) //Take input from user
+	fmt.Scanln(& n2) //Take input from user
 	result := n1 + n2
 	fmt.Println("Sum of two numbers: ", result)
 
