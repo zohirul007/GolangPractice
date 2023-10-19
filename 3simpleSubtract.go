e7881539c41153c39b285799c4decc317254e419
Zohirul Haque <zhremon@outlook.com>
2023-10-19 23:47:54 +0600

    simple subtraction

---
+++ 3simpleSubtract.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n1, n2, result = 77, 38, 0 // Declare variable and assign values
+	result = n1 - n2
+	fmt.Println("Subtraction of two numbers: ", result)
+
+}
