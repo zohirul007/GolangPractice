3c68b7a4c72e6ee59d4771593c2d4c9e76648c39
Zohirul Haque <zhremon@outlook.com>
2023-10-27 16:43:32 +0600

    same line declaration

--- 5sameLineDeclaration.go
+++ 5sameLineDeclaration.go
@@ -6,9 +6,10 @@ func main() {
 
 	age, name := 33,  "is the age of Tanjin Tisha"
 
-	// same line declaration
+	// We can declare multiple variables on the same line in Golang
 	// age := 33
-	// name := "The name of Tanjin Tisha" is the same as
+	// name := "The name of Tanjin Tisha"
+	// is the same as ⬇️
 	// age, name := 33,  "is the age of Tanjin Tisha"
 
 	fmt.Println(age, name)
