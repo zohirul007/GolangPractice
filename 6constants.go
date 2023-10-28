b8aa270ff2c4dfa8b22d7609a40fc2ac9b4f7224
Zohirul Haque <zhremon@outlook.com>
2023-10-28 07:29:20 +0600

    constant in go

---
+++ 6constants.go
@@ -0,0 +1,13 @@
+package main
+
+//Constants can be character, string, boolean, or numeric values. They can not be more complex types like slices, maps and structs
+import "fmt"
+
+func main() {
+
+	const Name = "Tanjin Tisha"
+	const Role = "is a Model and Tv actress"
+
+	fmt.Println("what is the name: ", Name)
+	fmt.Println("what is she doing: ", Role)
+}
