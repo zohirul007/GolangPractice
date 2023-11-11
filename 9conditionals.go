c9b6ce869904092506a1b1a9b7141f318b58de41
Zohirul Haque <zhremon@outlook.com>
2023-11-12 03:47:23 +0600

    Conditionals in go

---
+++ 9conditionals.go
@@ -0,0 +1,15 @@
+//If he has money in his account, calculate interest; if he doesn’t,
+//charge a penalty fee
+// solving with if condition
+package main
+import "fmt"
+
+func main(){
+	balance := -5
+
+	if balance < 0 {
+
+		fmt.Println("Your balance is below 0, add funds now or you will be charged a penalty.")
+		
+	}
+}
