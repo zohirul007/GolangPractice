914a6bdca6e82dafa873ddc5e2f1cf2b938a2ce2
Zohirul Haque <zhremon@outlook.com>
2023-12-27 10:09:31 +0600

    find grade in shortcut

---
+++ 8find_gradeCondtnhack.go
@@ -0,0 +1,28 @@
+/* If the student receives over 40% on her test, report that her grade passes;
+if not, report that her grade fails
+ with if statement. What if we want to have more than three possibilities, though?
+ We can do this by writing more than one else if statement into our code.
+find out A to F grade with using else if statement
+ 90 or above is equivalent to an A grade, 80-89 is equivalent to a B grade,
+ 70-79 is equivalent to a C grade, 65-69 is equivalent to a D grade
+ 64 or below is equivalent to an F grade
+ we can use conditional little hack to precise code  with if grade := 89  grade >=90  */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	if grade := 89; grade >= 90 {
+		fmt.Println("A grade")
+	} else if grade >= 80 {
+		fmt.Println("B grade")
+	} else if grade >= 70 {
+		fmt.Println("C grade")
+	} else if grade >= 65 {
+		fmt.Println("D grade")
+	} else {
+		fmt.Println("Failing grade")
+	}
+}
