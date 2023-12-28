be9be25a522057f3591eaefe928a618608b33dca
Zohirul Haque <zhremon@outlook.com>
2023-12-28 10:25:31 +0600

    If statement with simple tweaks

---
+++ 9B_statementhck.go
@@ -0,0 +1,15 @@
+//If he has money in his account, calculate interest; if he doesn’t,
+//charge a penalty fee
+// solving with if condition
+/* We can tweak this with shortcut if balance := -5; balance<0 */
+package main
+import "fmt"
+
+func main(){
+	
+	if balance := -5; balance < 0 {
+
+		fmt.Println("Your balance is below 0, add funds now or you will be charged a penalty.")
+		
+	}
+}
